608d914e3b39ba3dc3b76486bc4c3ce24eb03fcd
奋斗的小猴子 <2711845964@qq.com>
2022-03-03 11:02:07 +0800

    shiro验证

--- spring/springboot-demo/shiro-demo/src/main/java/com/xuwei/shiro/ShiroApplication.java
+++ spring/springboot-demo/shiro-demo/src/main/java/com/xuwei/shiro/ShiroApplication.java
@@ -1,13 +1,15 @@
 package com.xuwei.shiro;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @Description TODO
- * @Date 2022/2/28 16:09
+ * @Date 2022/3/2 9:53
  * @Author yxw
  */
+@MapperScan("com.xuwei.shiro.mapper")
 @SpringBootApplication
 public class ShiroApplication {
     public static void main(String[] args) {
